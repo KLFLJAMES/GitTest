cfb043ba3b6141598a2c8c94a6b972658ade30a7
fanzhou <779545222@qq.com>
2023-01-01 13:02:37 +0800

    初始化工程

---
+++ FZGitTest/FZGitTest/FZGitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FZGitTestApp.swift
+//  FZGitTest
+//
+//  Created by 范洲 on 2023/1/1.
+//
+
+import SwiftUI
+
+@main
+struct FZGitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
